6e3fdcc89835f07067b038c77b2d93516d089021
Yulchan Shin <yulchanshin@gmail.com>
2025-08-16 14:20:08 -0400

    added single search feature

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-document.addEventListener("DOMContentLoaded", fetchOnPageLoad)
-
-
+const searchBtn = document.getElementById("search-btn");
 const resultContainer = document.getElementById("results-container");
+const searchBar = document.getElementById("search-bar");
+const detailsContainer = document.getElementById("details-container")
 
+document.addEventListener("DOMContentLoaded", fetchOnPageLoad);
+searchBtn.addEventListener("click", fetchOnSearch);
+searchBar.addEventListener("keydown", (event)=>{
+    if (event.key === "Enter"){
+        fetchOnSearch();
+    }
+})
+
+
+//function to send the user to the link when click on view recipe button
 function playVideo (link){
-    window.open(link)
+    window.open(link);
 }
 
-
-
+//whats loaded when the DOM is initially laoded
 async function fetchOnPageLoad(){
-
     try{
+        resultContainer.style.display = "grid"
         let i = 0;
         let noDupe = [];
         let cardsArr = []; //This array is to save all the html element and display them all at once later
@@ -34,6 +43,7 @@ async function fetchOnPageLoad(){
             const mealName = meal.strMeal;
             const thumbnail = meal.strMealThumb;
             const link = meal.strYoutube;
+
             cardsArr.push(`
                 <div class="card">
                     <p class="meal-name">${mealName}</p>
@@ -47,4 +57,71 @@ async function fetchOnPageLoad(){
     catch(error){
         console.error(error);
     }
+}
+
+//whats loaded when the search has been made
+async function fetchOnSearch(){
+    resultContainer.innerHTML = "";
+    resultContainer.style.display = "block";
+    try{
+        const inputVal = document.getElementById("search-bar").value.toLowerCase();
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputVal}`)
+
+        if (!response.ok){
+            throw new Error("Could not fetch resource")
+        }
+
+        const data = await response.json();
+        if (!data.meals){ //in case the menu does not exist
+            fetchOnPageLoad();
+            alert("The Recipe for this food does not exist in our database");
+            return
+        }
+
+        const meal = data.meals[0]
+        const mealId = meal.idMeal;
+
+        //adding a video directly into the resultContainer frame as well as two span for ingredients + instructions to be added
+        const videoId = meal.strYoutube.split("v=")[1].split("&")[0];
+        const embedLink = `https://www.youtube.com/embed/${videoId}`
+        
+        resultContainer.innerHTML = `
+            
+            <div class="vid-wrapper">
+                <iframe class="meal-video" src="${embedLink}" allowfullscreen></iframe>
+            </div>
+            <div class="rest-wrapper"> 
+                <span id="instruction-wrapper">
+                    <h2>Instructions</h2>
+                </span>
+                <span id="ingredients-wrapper">
+                    <h2>Ingredients</h2>
+                </span>
+            </div>
+        `
+
+
+        const instructions = meal.strInstructions;
+    
+        //forloop to store all VALID ingredients in a new array
+        let ingredientsArr = []
+        for (let i = 1; i <= 20; i++){
+            const ingredient = meal[`strIngredient${i}`]
+            if (!ingredient || ingredient === ""){
+                continue;
+            } 
+            ingredientsArr.push(`${ingredientsArr.length + 1}. ${ingredient} <br>`)
+        }
+        
+        const instructionWrapper = document.getElementById("instruction-wrapper");
+        const ingredientsWrapper = document.getElementById("ingredients-wrapper");
+        instructionWrapper.innerHTML += instructions.replace(/\n\r?/g, "<br>"); //regex to replace the line breaks
+        ingredientsWrapper.innerHTML += ingredientsArr.join("")
+
+    }
+
+    catch(error){
+        console.error(error)
+    }
+
 }
